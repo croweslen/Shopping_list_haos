f8b3063108bfd4e4e552e4d49e3a84e1bddb0dfc
croweslen <dabate828@gmail.com>
2026-07-15 15:29:31 -0400

    resetttt

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass, config):
+    _LOGGER.info("Multi-list shopping integration is loading!")
+    return True
--- custom_components/multi_list/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging  
-
-async def async_setup(hass, config):
-    #code here 
-    return True
