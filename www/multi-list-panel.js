db2890b8aa29b3f47b7d80405f4b705f6cb754d8
croweslen <dabate828@gmail.com>
2026-08-04 09:37:37 -0400

    js fixes

--- www/multi-list-panel.js
+++ www/multi-list-panel.js
@@ -1,6 +1,21 @@
 import { LitElement, html, css } from "https://unpkg.com/lit@2.7.0/index.js?module";
 
 class MultiListPanel extends LitElement {
+  static properties = {
+    hass: { type: Object },
+  };
+
+  static styles = css`
+    :host {
+      display: block;
+      padding: 24px;
+      background: var(--primary-background-color, #1c1c1c);
+      color: var(--primary-text-color, #fff);
+      min-height: 100vh;
+      box-sizing: border-box;
+    }
+  `;
+
   render() {
     return html`<h1>Hello Multi List</h1>`;
   }
