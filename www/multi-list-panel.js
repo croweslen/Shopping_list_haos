7bb58f3fed55a6582f7fb55da8f9b171eb82bce8
root <root@a0d7b954-vscode.local.hass.io>
2026-08-04 09:23:48 -0400

    add frontend js

---
+++ www/multi-list-panel.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from "https://unpkg.com/lit@2.7.0/index.js?module";
+
+class MultiListPanel extends LitElement {
+  render() {
+    return html`<h1>Hello Multi List</h1>`;
+  }
+}
+
+customElements.define("multi-list-panel", MultiListPanel);
