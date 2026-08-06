6fac61e45e90fbff4453d0d01ce2c2e2e1fcbf82
croweslen <dabate828@gmail.com>
2026-08-06 09:32:22 -0400

    gap fix>

--- www/multi-list-panel.js
+++ www/multi-list-panel.js
@@ -20,7 +20,7 @@ class MultiListPanel extends LitElement {
     .menu{
       display: flex;
       flex-direction: column;
-      gap: 16px;
+      gap: 25px;
       max-width: 480px;
 
     }
@@ -55,7 +55,6 @@ render() {
       <ha-icon icon="mdi:cart"></ha-icon>
       Store Manager
     </button>
-    <br>
     <button class="menu-card" @click=${() => this._navigate("listManager")}>
       <ha-icon icon="mdi:cart"></ha-icon>
       List Manager
